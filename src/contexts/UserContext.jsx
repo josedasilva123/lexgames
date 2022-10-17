14626bc97c866e679a28b8c126015787603f5f2b
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-10-17 17:49:31 -0300

    fix: fixing toast texts

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -9,82 +9,101 @@ export const UserContext = createContext({});
 /* Cria o contexto, exporta lógica por meio do provider */
 
 export const UserProvider = ({ children }) => {
-    const [globalLoading, setGlobalLoading] = useState();
-    const [user, setUser] = useState(null);
-    const [favoriteList, setFavoriteList] = useState([]);
-    const [currentRoute, setCurrentRoute] = useState(null);
+  const [globalLoading, setGlobalLoading] = useState();
+  const [user, setUser] = useState(null);
+  const [favoriteList, setFavoriteList] = useState([]);
+  const [currentRoute, setCurrentRoute] = useState(null);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-      (async () => {
-        const token = localStorage.getItem("@TOKEN");
-        if(token){
-          setGlobalLoading(true);
-          try {
-            const response = await coreApi.get("user/autologin", {
-              headers: {
-                auth: token,
-              }
-            })
-            setUser(response.data.user);
-            setFavoriteList(response.data.user.favoriteGames);
-            navigate(currentRoute ? currentRoute : "/dashboard");
-          } catch (error) {
-            localStorage.removeItem("@TOKEN");
-            navigate("/");
-          } finally {
-            setGlobalLoading(false);
-          }
+  useEffect(() => {
+    (async () => {
+      const token = localStorage.getItem("@TOKEN");
+      if (token) {
+        setGlobalLoading(true);
+        try {
+          const response = await coreApi.get("user/autologin", {
+            headers: {
+              auth: token,
+            },
+          });
+
+          setUser(response.data.user);
+          setFavoriteList(response.data.user.favoriteGames);
+          navigate(currentRoute ? currentRoute : "/dashboard");
+          
+        } catch (error) {
+          localStorage.removeItem("@TOKEN");
+          navigate("/");
+        } finally {
+          setGlobalLoading(false);
         }
-      })();
-    }, [])
+      }
+    })();
+  }, []);
 
-    /* Botão de login */
-    const userLogin = async (data, setLoading) => {
-        try {
-            setLoading(true);
-            const response = await coreApi.post("user/login", data);
-            setUser(response.data.user);
-            setFavoriteList(response.data.user.favoriteGames);
-            localStorage.setItem("@TOKEN", response.data.token);
-            navigate("/dashboard");
-          } catch (error) {
-            console.log(error);
-            toast.error(error.response.data.error);
-          } finally {
-            setLoading(false);
-          }
+  /* Botão de login */
+  const userLogin = async (data, setLoading, callback) => {
+    try {
+      setLoading(true);
+      const response = await coreApi.post("user/login", data);
+
+      setUser(response.data.user);
+      setFavoriteList(response.data.user.favoriteGames);
+      
+      localStorage.setItem("@TOKEN", response.data.token);
+      navigate("/dashboard");
+      if(callback){
+        callback(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    const userLogout = () => {
-        setUser(null);
-        setFavoriteList([]);
+  const userLogout = () => {
+    setUser(null);
+    setFavoriteList([]);
+    navigate("/");
+    localStorage.removeItem("@TOKEN");
+  };
+
+  const userRegister = async (data, setLoading) => {
+    try {
+      setLoading(true);
+      const response = await coreApi.post("user", data);
+
+      toast.success(response.data.message);
+
+      setTimeout(() => {
         navigate("/");
-        localStorage.removeItem("@TOKEN");
+      }, 2500);
+    } catch (error) {
+      toast.error(error.response.data.error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    const userRegister = async(data, setLoading) => {
-        try {
-            setLoading(true);
-            const response = await coreApi.post("user", data);
-      
-            toast.success(response.data.message);
-      
-            setTimeout(() => {
-              navigate("/");
-            }, 2500);
-          } catch (error) {
-            toast.error(error.response.data.error);
-          } finally {
-            setLoading(false);
-          }
-    }
-
-    /* export */
-    return(
-        <UserContext.Provider value={{ user, favoriteList, setFavoriteList, currentRoute, setCurrentRoute, userLogin, userRegister, userLogout, globalLoading }}>
-            {children}
-        </UserContext.Provider>
-    )
-}
+  /* export */
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        favoriteList,
+        setFavoriteList,
+        currentRoute,
+        setCurrentRoute,
+        userLogin,
+        userRegister,
+        userLogout,
+        globalLoading,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
